e89344840a7fc2ed9b9e343fd7d425be6ec1ba1b
ronapendol11 <45029821+ronapendol11@users.noreply.github.com>
2018-11-14 15:00:11 +0700

    hostpot login for wifi
    contact me

---
+++ Copyright.js
@@ -0,0 +1,12 @@
+﻿var mydate=new Date()
+var year=mydate.getYear()
+if (year < 1000)
+year+=1900
+var day=mydate.getDay()
+var month=mydate.getMonth()+1
+if (month<10)
+month="0"+month
+var daym=mydate.getDate()
+if (daym<10)
+daym="0"+daym
+document.write("<b>"+year+"</b>")
